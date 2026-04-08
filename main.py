1466b15d856c9f90c50695d6872d51ae9869097d
Корзун Олександр <olexandr.korzun@gmail.com>
2026-04-08 18:35:53 +0300

    Add logic and file writing

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         return set(line.strip() for line in f)
+
+
+def process_files(file1, file2):
+    lines1 = read_file(file1)
+    lines2 = read_file(file2)
+
+    same = lines1.intersection(lines2)
+    diff = lines1.symmetric_difference(lines2)
+
+    with open('same.txt', 'w', encoding='utf-8') as f:
+        f.write('\n'.join(sorted(same)))
+
+    with open('diff.txt', 'w', encoding='utf-8') as f:
+        f.write('\n'.join(sorted(diff)))
